77101367b8c50c1f8e2d4840fab62f6864b5def5
Absolute-Value <ji.kei1220@gmail.com>
2023-07-29 16:24:46 +0900

    整理

--- paint.py
+++ paint.py
@@ -5,23 +5,24 @@ import tkinter
 from functools import partial
 
 class Paint:
-    def __init__(self):
+    def __init__(self, root):
         # 操作中の図形のID
         self.curr_id = -1
         self.color = "black"
 
         # メインウィンドウ作成
-        root = tkinter.Tk()
         root.title("ペイントソフト")
 
         #  画像表示用キャンバス作成
         self.canvas = tkinter.Canvas(root, width=800, height=600, bg="white")
+        self.canvas.pack(expand=True, fill=tkinter.BOTH)
         # キーバインド
         self.canvas.bind("<ButtonPress-1>", self.on_mouse_left)
         self.canvas.bind("<B1-Motion>", self.dragging)
 
-        # ボタン配置用フレーム作成
+        # ツール用フレーム作成
         self.button_frame = tkinter.Frame(root)
+        self.button_frame.pack()
 
         # カラーパレット
         self.color_buttons = []
@@ -32,10 +33,6 @@ class Paint:
         # ボタン配置
         self.clear_button = tkinter.Button(self.button_frame, text="クリア", command=self.press_clear_button)
         self.clear_button.grid(row=0,column=5)
-
-        #レイアウト
-        self.canvas.pack(expand=True, fill=tkinter.BOTH)
-        self.button_frame.pack()
 
         #ウィンドウの表示
         root.mainloop()
@@ -57,4 +54,6 @@ class Paint:
     def press_clear_button(self):
         self.canvas.delete('all')
 
-Paint()
+if __name__ == "__main__":
+    root = tkinter.Tk()
+    app = Paint(root)
